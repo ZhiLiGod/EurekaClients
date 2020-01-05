7962eca93520f98eb90a9502b546c8f8da04ad81
Zhi Li <zhi.li@invoicefair.com>
2020-01-05 23:23:58 +0000

    configed bulkhead

--- movie-catalog-service/src/main/java/com/eureka/client/moviecatalogservice/services/impl/RatingsServiceImpl.java
+++ movie-catalog-service/src/main/java/com/eureka/client/moviecatalogservice/services/impl/RatingsServiceImpl.java
@@ -10,6 +10,7 @@ import com.eureka.client.moviecatalogservice.dtos.RatingDto;
 import com.eureka.client.moviecatalogservice.dtos.RatingsDto;
 import com.eureka.client.moviecatalogservice.services.RatingsService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @Service
 public class RatingsServiceImpl implements RatingsService {
@@ -17,8 +18,15 @@ public class RatingsServiceImpl implements RatingsService {
   @Autowired
   private RestTemplate restTemplate;
 
+  // @formatter:off
   @Override
-  @HystrixCommand(fallbackMethod = "getFallBackRatings")
+  @HystrixCommand(fallbackMethod = "getFallBackRatings"
+      , threadPoolKey = "ratingsPool"
+      , threadPoolProperties = {
+          @HystrixProperty(name = "coreSize", value = "20"),
+          @HystrixProperty(name = "maxQueueSize", value = "10")
+      })
+  // @formatter:on
   public RatingsDto getRatings() {
     return restTemplate.getForObject("http://rate-info-service:8083/rating-info-api/", RatingsDto.class);
   }
