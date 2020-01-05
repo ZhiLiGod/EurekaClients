296e51e47df291abec17bf1ddfb869de04f5dc82
Zhi Li <zhi.li@invoicefair.com>
2020-01-05 18:25:05 +0000

    updated gitignore

--- movie-catalog-service/src/test/java/com/eureka/client/moviecatalogservice/MovieCatalogServiceApplicationTests.java
+++ movie-catalog-service/src/test/java/com/eureka/client/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -1,13 +1,13 @@
 package com.eureka.client.moviecatalogservice;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MovieCatalogServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
