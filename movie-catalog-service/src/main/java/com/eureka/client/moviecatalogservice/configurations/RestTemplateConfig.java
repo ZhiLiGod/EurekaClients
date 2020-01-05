1fdfc77d94d8a755a38e518a559f353e433dee04
Zhi Li <zhi.li@invoicefair.com>
2020-01-05 21:42:14 +0000

    added timeout on resttemplate

--- movie-catalog-service/src/main/java/com/eureka/client/moviecatalogservice/configurations/RestTemplateConfig.java
+++ movie-catalog-service/src/main/java/com/eureka/client/moviecatalogservice/configurations/RestTemplateConfig.java
@@ -3,15 +3,21 @@ package com.eureka.client.moviecatalogservice.configurations;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
 
+  private static final int TIMEOUT = 3000;
+
   @Bean
   @LoadBalanced
   public RestTemplate getRestTemplate() {
-    return new RestTemplate();
+    HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+    factory.setConnectTimeout(TIMEOUT);
+
+    return new RestTemplate(factory);
   }
 
 }
